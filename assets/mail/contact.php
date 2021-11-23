008da77daff5ea22a70a6a3b77a38be70f9a62a7
Dexeus1260 <73435579+Dexeus1260@users.noreply.github.com>
2021-11-23 17:46:37 +0800

    contact and about

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'admin/functions/functions.php';
 
     header("Access-Control-Allow-Origin: *");
     // Only process POST reqeusts.
@@ -40,17 +41,19 @@
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo set_message(display_success("Thank You! Your message has been sent."));
+
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo set_message(display_error("Oops! Something went wrong and we couldn't send your message."));
+          
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo set_message(display_error("There was a problem with your submission, please try again."));
     }
 
 ?>
